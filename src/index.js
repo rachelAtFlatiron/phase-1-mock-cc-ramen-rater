3e261094cb56bebb7714bc1a2463232925f5dd8c
Rachel Katz <rachel.katz@flatironschool.com>
2022-07-27 13:10:07 -0400

    set to core deliverables

--- src/index.js
+++ src/index.js
@@ -12,10 +12,8 @@ const itemName = document.querySelector("#ramen-detail .name")
 const rating = document.querySelector('#rating-display')
 const comment = document.querySelector('#comment-display')
 
-//forms and delete button
+//form
 const newForm = document.querySelector('#new-ramen')
-const editForm = document.querySelector('#edit-ramen')
-const deleteBtn = document.querySelector('#delete-ramen')
 
 //url for localhost
 const url = 'http://localhost:3000/ramens'
@@ -37,79 +35,12 @@ const addRamen = (ramen) => {
 //show ramen in details div
 //ramen parameter takes an object representing a ramen
 const showRamen = (ramen) => {
-    //including new attribute ramen-id for editing and deleting
-    let ramenId = ramen.id;
-    ramenDetail.setAttribute('ramen-id', ramenId); 
-    deleteBtn.setAttribute('ramen-id', ramenId);
-    editForm.setAttribute('ramen-id', ramenId);
-
     //update details div with ramen info
     restaurant.innerText = ramen.restaurant;
     itemName.innerText = ramen.name;
     rating.innerText = ramen.rating;
     comment.innerText = ramen.comment;
     image.src = ramen.image;
-}
-
-
-
-//deletes ramen from database and removes from ramen menu
-//e parameter takes on click event object
-const deleteRamen = (e) => {
-    //e.target is the delete button
-    let rmId = e.target.getAttribute('ramen-id') //get current ramen id from delete button
-    //contains all menu images
-    let menuList = document.querySelectorAll('#ramen-menu img') //get list of all images in menu
-
-    //update database
-    fetch(`${url}/${rmId}`, {
-        method: 'DELETE'
-    })
-    .then(res => res.json())
-    .then(() => { 
-            //remove from DOM here so we know it was deleted from server
-            //iterate through menu images until i find matching ids
-            menuList.forEach((el, i) => {
-            if (el.getAttribute('ramen-id') === rmId) {
-                el.remove(); //remove image node from menuList
-            }
-        })
-    })
-    .catch(err => console.log(err));
-}
-
-//edit ramen from database using PATCH and updates in details div
-//e parameter takes form submit event object
-const editRamen = (e) => {
-
-    e.preventDefault();
-    let newRating = e.target.rating.value;
-    let newComment = e.target['new-comment'].value;
-    let ramenId = e.target.getAttribute('ramen-id'); //get ramen id for fetch statement
-    
-    //with PATCH we don't need to include all key/value pairs
-    let body = {
-        rating: newRating,
-        comment: newComment
-    }
-
-    //patch
-    fetch(`${url}/${ramenId}`, {
-        method: 'PATCH',
-        body: JSON.stringify(body),
-        headers: {
-            'Content-Type': 'application/json'
-        }
-    })
-    .then(res => res.json())
-    .then(data => { //update DOM here to get exactly what was update in server
-        rating.innerText = data.rating;
-        comment.innerText = data.comment;
-    })
-    .catch(err => console.log(err));
-
-    //reset form
-    e.target.reset();
 }
 
 //adds new ramen to database and updates ramen menu, and shows in details div
@@ -126,39 +57,26 @@ const newRamen = (e) => {
         comment: e.target['new-comment'].value
     }
 
-    //post
-    fetch(url, {
-        method: 'POST',
-        body: JSON.stringify(newRamen),
-        headers: {
-            'Content-Type': 'application/json'
-        }
-    })
-    .then(res => res.json())
-    .then(data => {
-        addRamen(data); //add to ramen menu
-        showRamen(data); //show new ramen in details div immediately
-    })
-    .catch(err => console.log(err))
+    addRamen(newRamen);
+    showRamen(newRamen);
 
     //reset form
     e.target.reset();
 }
 
 //add event listeners using methods above
-deleteBtn.addEventListener('click', deleteRamen);
-editForm.addEventListener('submit', editRamen);
 newForm.addEventListener('submit', newRamen);
 
-//when dom is loaded fetch data, return first ramen, and showRamen
-document.addEventListener('DOMContentLoaded', (e) => {
-    fetch(url)
+//fetch data, return first ramen, and showRamen
+fetch(url)
     .then(res => res.json())
     .then(data => {
         data.forEach((el, i) => {
             addRamen(el);
         })
-        return data[0] //return first ramen to next then
+        return data[0] //return first ramen to next '.then'
     })
     .then(firstRamen => showRamen(firstRamen)) 
-})
+    .catch(err => console.log(err))
+
+//NOTE: DOMContentLoaded is taken care of with 'defer' keyword in script tag of index.html
